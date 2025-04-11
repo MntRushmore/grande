20326a1a56b3b9aa30c4321f40015dc8f1acbef8
WolfGamer2 <rushilchopra@gmail.com>
2025-04-11 13:20:17 -0700

    new stuff

--- api/slack/events.js
+++ api/slack/events.js
@@ -1,19 +1,34 @@
-const { App, ExpressReceiver } = require('@slack/bolt');
+const { WebClient } = require('@slack/web-api');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
+const client = new WebClient(process.env.SLACK_BOT_TOKEN, { fetch });
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  receiver,
-  clientOptions: {
-    fetch,
-  },
-});
+module.exports = async ({ ack, body }) => {
+  await ack();
 
-app.command('/grant', require('../../commands/grant'));
-
-// Export the receiver app to handle requests from Vercel
-module.exports = receiver.app;
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: 'grant_modal',
+      title: { type: 'plain_text', text: 'Send Card Grant' },
+      submit: { type: 'plain_text', text: 'Send' },
+      close: { type: 'plain_text', text: 'Cancel' },
+      blocks: [
+        {
+          type: 'section',
+          text: { type: 'mrkdwn', text: '*Fill out the grant details below*' },
+        },
+        {
+          type: 'input',
+          block_id: 'amount_block',
+          element: {
+            type: 'plain_text_input',
+            action_id: 'amount',
+          },
+          label: { type: 'plain_text', text: 'Amount ($)' },
+        },
+      ],
+    },
+  });
+};
